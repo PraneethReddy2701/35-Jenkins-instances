1f1069100f48970c6fa4a9dc92cd9937d934db0c
Praneeth Reddy <praneeth.kotholla@gmail.com>
2026-03-12 16:35:56 +0530

    jenkins instances

---
+++ main.tf
@@ -0,0 +1,90 @@
+# Jenkins instance
+resource "aws_instance" "jenkins" {
+  ami           = local.ami_id
+  instance_type = "t3.small"
+  vpc_security_group_ids = [aws_security_group.main.id]
+  subnet_id = "subnet-03e3e53e1e2fda2ce" #replace your Subnet
+
+  # need more for terraform
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+  user_data = file("jenkins.sh")
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-jenkins"
+    }
+  )
+}
+
+# Jenkins-agent
+resource "aws_instance" "jenkins_agent" {
+  ami           = local.ami_id
+  instance_type = "t3.small"
+  vpc_security_group_ids = [aws_security_group.main.id]
+  subnet_id = "subnet-03e3e53e1e2fda2ce" #replace your Subnet
+
+  # need more for terraform
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+  user_data = file("jenkins-agent.sh")
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-jenkins-agent"
+    }
+  )
+}
+
+# Security group
+resource "aws_security_group" "main" {
+  name        =  "${var.project}-${var.environment}-jenkins"
+  description = "Created to attatch Jenkins and its agents"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-jenkins"
+    }
+  )
+}
+
+# R53 record for jenkins
+resource "aws_route53_record" "jenkins" {
+  zone_id = var.zone_id
+  name    = "jenkins.${var.zone_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.jenkins.public_ip]
+  allow_overwrite = true
+}
+
+# R53 record for jenkins-agent
+resource "aws_route53_record" "jenkins-agent" {
+  zone_id = var.zone_id
+  name    = "jenkins-agent.${var.zone_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.jenkins_agent.private_ip]
+  allow_overwrite = true
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "bittu27.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z0299510X8GXT0G847SE"
+  description = "description"
+}
