4cd8172d7658026590f2be1d9f65e93a42c2a6f3
Praneeth Reddy <praneeth.kotholla@gmail.com>
2026-03-12 16:56:22 +0530

    updated jenkins userdata file

--- jenkins.sh
+++ jenkins.sh
@@ -12,10 +12,30 @@ xfs_growfs /var/tmp
 xfs_growfs /var
 
 # Install Jenkins
-curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+curl -L -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
 yum install fontconfig java-21-openjdk -y
+
 yum install jenkins -y
+
 systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
+
+
+
+# Install Jenkins
+# curl -L -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# yum install -y fontconfig java-21-openjdk
+
+# yum clean all
+# yum makecache
+
+# yum install -y jenkins
+
+# systemctl daemon-reload
+# systemctl enable jenkins
+# systemctl start jenkins
